0241a25c3b3497fb3b17021bb31442f9070dfcca
Nadiia Mishchuk <n.mishchuk@apiko.com>
2024-04-18 16:11:06 +0300

    connect toQueryClientProvider

---
+++ src/api/api.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const url = process.env.REACT_APP_API_URL
+
+export const api = axios.create({
+    baseURL: url,
+    headers: {
+        'Content-Type': 'application/json',
+        Accept: 'application/json',
+        withAuth: true,
+    },
+});
